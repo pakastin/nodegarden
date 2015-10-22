ef1b5886a8dc97ad55706ab2ed724e550266934e
Juha Lindstedt <juha@pakastin.fi>
2015-10-22 13:29:01 +0300

    add comments

--- public/js/main-dev.js
+++ public/js/main-dev.js
@@ -3,9 +3,9 @@
 (function () {
   'use strict';
 
-  var scale = window.devicePixelRatio;
-  var wWidth = window.innerWidth * scale;
-  var wHeight = window.innerHeight * scale;
+  var pixelRatio = window.devicePixelRatio;
+  var wWidth = window.innerWidth * pixelRatio;
+  var wHeight = window.innerHeight * pixelRatio;
   var wArea = wWidth * wHeight;
 
   var nodes = new Array(Math.sqrt(wArea) / 10 | 0);
@@ -15,8 +15,9 @@
 
   var $container = document.getElementById('container');
 
-  if (scale !== 1) {
-    canvas.style.transform = 'scale(' + 1 / scale + ')';
+  if (pixelRatio !== 1) {
+    // if retina screen, scale canvas
+    canvas.style.transform = 'scale(' + 1 / pixelRatio + ')';
     canvas.style.transformOrigin = '0 0';
   }
   canvas.id = 'nodegarden';
@@ -27,8 +28,8 @@
   render();
 
   window.addEventListener('resize', function () {
-    wWidth = window.innerWidth * scale;
-    wHeight = window.innerHeight * scale;
+    wWidth = window.innerWidth * pixelRatio;
+    wHeight = window.innerHeight * pixelRatio;
     wArea = wWidth * wHeight;
     nodes.length = Math.sqrt(wArea) / 25 | 0;
     init();
@@ -39,6 +40,7 @@
     canvas.width = wWidth;
     canvas.height = wHeight;
 
+    // create nodes
     var i, len;
     for (i = 0, len = nodes.length; i < len; i++) {
       if (nodes[i]) {
@@ -57,13 +59,15 @@
   }
 
   function render() {
-    requestAnimationFrame(render);
     var distance;
     var direction;
     var force;
     var xForce, yForce;
     var xDistance, yDistance;
     var i, j, nodeA, nodeB, len;
+
+    // request new animationFrame
+    requestAnimationFrame(render);
 
     // clear canvas
     ctx.clearRect(0, 0, wWidth, wHeight);
@@ -76,9 +80,11 @@
         xDistance = nodeB.x - nodeA.x;
         yDistance = nodeB.y - nodeA.y;
 
+        // calculate distance
         distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 
         if (distance < Math.max(nodeA.m, nodeB.m)) {
+          // collision: remove smaller or equal
           if (nodeA.m <= nodeB.m) {
             nodeA.x = Math.random() * wWidth;
             nodeA.y = Math.random() * wHeight;
@@ -96,20 +102,25 @@
         }
 
         if (distance > 200) {
+          // distance over 200 pixels - ignore gravity
           continue;
         }
 
+        // calculate gravity direction
         direction = {
           x: xDistance / distance,
           y: yDistance / distance
         };
 
+        // calculate gravity force
         force = 10 * nodeA.m * nodeB.m / Math.pow(distance, 2);
 
         if (force > 0.025) {
+          // cap force to a maximum value of 0.025
           force = 0.025;
         }
 
+        // draw gravity lines
         ctx.beginPath();
         ctx.strokeStyle = 'rgba(63,63,63,' + force * 40 + ')';
         ctx.moveTo(nodeA.x, nodeA.y);
@@ -119,6 +130,7 @@
         xForce = force * direction.x;
         yForce = force * direction.y;
 
+        // calculate new velocity after gravity
         if (nodeA.pos !== nodeB.pos) {
           nodeA.vx -= xForce;
           nodeA.vy -= yForce;
@@ -144,6 +156,7 @@
       nodes[i].y += nodes[i].vy;
 
       if (nodes[i].x > wWidth + 25 || nodes[i].x < -25 || nodes[i].y > wHeight + 25 || nodes[i].y < -25) {
+        // if node over screen limits - reset to a init position
         nodes[i].x = Math.random() * wWidth;
         nodes[i].y = Math.random() * wHeight;
         nodes[i].vx = Math.random() * 2 - 1;
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 
-var scale = window.devicePixelRatio
-var wWidth = window.innerWidth * scale
-var wHeight = window.innerHeight * scale
+var pixelRatio = window.devicePixelRatio
+var wWidth = window.innerWidth * pixelRatio
+var wHeight = window.innerHeight * pixelRatio
 var wArea = wWidth * wHeight
 
 var nodes = new Array(Math.sqrt(wArea) / 10 | 0)
@@ -11,8 +11,9 @@ var ctx = canvas.getContext('2d')
 
 var $container = document.getElementById('container')
 
-if (scale !== 1) {
-  canvas.style.transform = 'scale(' + 1 / scale + ')'
+if (pixelRatio !== 1) {
+  // if retina screen, scale canvas
+  canvas.style.transform = 'scale(' + 1 / pixelRatio + ')'
   canvas.style.transformOrigin = '0 0'
 }
 canvas.id = 'nodegarden'
@@ -23,8 +24,8 @@ init()
 render()
 
 window.addEventListener('resize', function () {
-  wWidth = window.innerWidth * scale
-  wHeight = window.innerHeight * scale
+  wWidth = window.innerWidth * pixelRatio
+  wHeight = window.innerHeight * pixelRatio
   wArea = wWidth * wHeight
   nodes.length = Math.sqrt(wArea) / 25 | 0
   init()
@@ -35,6 +36,7 @@ function init () {
   canvas.width = wWidth
   canvas.height = wHeight
 
+  // create nodes
   var i, len
   for (i = 0, len = nodes.length; i < len; i++) {
     if (nodes[i]) {
@@ -53,13 +55,15 @@ function init () {
 }
 
 function render () {
-  requestAnimationFrame(render)
   var distance
   var direction
   var force
   var xForce, yForce
   var xDistance, yDistance
   var i, j, nodeA, nodeB, len
+
+  // request new animationFrame
+  requestAnimationFrame(render)
 
   // clear canvas
   ctx.clearRect(0, 0, wWidth, wHeight)
@@ -72,9 +76,11 @@ function render () {
       xDistance = nodeB.x - nodeA.x
       yDistance = nodeB.y - nodeA.y
 
+      // calculate distance
       distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
 
       if (distance < Math.max(nodeA.m, nodeB.m)) {
+        // collision: remove smaller or equal
         if (nodeA.m <= nodeB.m) {
           nodeA.x = Math.random() * wWidth
           nodeA.y = Math.random() * wHeight
@@ -92,20 +98,25 @@ function render () {
       }
 
       if (distance > 200) {
+        // distance over 200 pixels - ignore gravity
         continue
       }
 
+      // calculate gravity direction
       direction = {
         x: xDistance / distance,
         y: yDistance / distance
       }
 
+      // calculate gravity force
       force = (10 * nodeA.m * nodeB.m) / Math.pow(distance, 2)
 
       if (force > 0.025) {
+        // cap force to a maximum value of 0.025
         force = 0.025
       }
 
+      // draw gravity lines
       ctx.beginPath()
       ctx.strokeStyle = 'rgba(63,63,63,' + force * 40 + ')'
       ctx.moveTo(nodeA.x, nodeA.y)
@@ -115,6 +126,7 @@ function render () {
       xForce = force * direction.x
       yForce = force * direction.y
 
+      // calculate new velocity after gravity
       if (nodeA.pos !== nodeB.pos) {
         nodeA.vx -= xForce
         nodeA.vy -= yForce
@@ -140,6 +152,7 @@ function render () {
     nodes[i].y += nodes[i].vy
 
     if (nodes[i].x > wWidth + 25 || nodes[i].x < -25 || nodes[i].y > wHeight + 25 || nodes[i].y < -25) {
+      // if node over screen limits - reset to a init position
       nodes[i].x = Math.random() * wWidth
       nodes[i].y = Math.random() * wHeight
       nodes[i].vx = Math.random() * 2 - 1
